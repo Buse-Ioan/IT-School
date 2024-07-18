47ab1579be996edd99145128227d65bdc17a1c7f
buse_ <buse.v.ioan@gmail.com>
2024-07-18 20:01:33 +0300

    update project

---
+++ src/main/java/session_13_abstraction/practice/data_information_system/Report.java
@@ -0,0 +1,24 @@
+package session_13_abstraction.practice.data_information_system;
+
+import java.util.List;
+
+public abstract class Report {
+
+    //common report properties
+    protected String title;
+    protected List<String> data;
+
+    Report(String title, List<String> data) {
+        this.title = title;
+        this.data = data;
+    }
+
+    public abstract void exportToPdf();
+
+    //common report behavior
+    public abstract void showPreview();
+
+    public void displayMetadata() {
+        System.out.println("Title: " + title);
+    }
+}
